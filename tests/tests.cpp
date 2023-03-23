06a322491357e1ed025419ae8f0bba46eb3c13ed
jezzixxx <pkapav02@gmail.com>
2023-03-23 19:24:43 +0300

    try4

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,25 +3,26 @@
 #include <gtest/gtest.h>
 
 TEST(Transaction, Banking){
-        Account test_account0(0,1000), test_account1(1,500);
-        Transaction test_transaction;
-        ASSERT_EQ(test_transaction.fee(), 1);
-        test_transaction.set_fee(100);
-        ASSERT_EQ(test_transaction.fee(), 100);
-        ASSERT_THROW(test_transaction.Make(test_account0, test_account0, 500), std::logic_error);
-        ASSERT_THROW(test_transaction.Make(test_account0, test_account1, -500), std::invalid_argument);
-        ASSERT_THROW(test_transaction.Make(test_account0, test_account1, 50), std::logic_error);
-        if (test_transaction.fee()*2-1 >= 100)
-                ASSERT_EQ(test_transaction.Make(test_account0, test_account1, test_transaction.fee()*2-1), false);
-        test_account0.Lock();
-        ASSERT_THROW(test_transaction.Make(test_account0, test_account1, 1000), std::runtime_error);
-        test_account0.Unlock();
-        ASSERT_EQ(test_transaction.Make(test_account0, test_account1, 500), true);
-        ASSERT_EQ(test_account1.GetBalance(), 1000);
-        ASSERT_EQ(test_account0.GetBalance(), 400);
-        ASSERT_EQ(test_transaction.Make(test_account0, test_account1, 400), false);
-        ASSERT_EQ(test_account1.GetBalance(), 1000);
-        ASSERT_EQ(test_account0.GetBalance(), 400);
+	const int num0 = 5000, num1 = 5000, num_fee = 100;
+	Account user0(0,num0), user1(1,num1);
+	Transaction test_transaction;
+	ASSERT_EQ(test_transaction.fee(), 1);
+	test_transaction.set_fee(num_fee);
+	ASSERT_EQ(test_tran.fee(), base_fee);
+	ASSERT_THROW(test_transaction.Make(user0, user0, 1000), std::logic_error);
+	ASSERT_THROW(test_transaction.Make(user0, user1, -50), std::invalid_argument);
+	ASSERT_THROW(test_transaction.Make(user0, user1, 50), std::logic_error);
+	if (test_transaction.fee()*2-1 >= 100)
+		ASSERT_EQ(test_transaction.Make(user0, user1, test_transaction.fee()*2-1), false);
+	user0.Lock();
+	ASSERT_THROW(test_transaction.Make(user0, user1, 1000), std::runtime_error);
+	user0.Unlock();
+	ASSERT_EQ(test_transaction.Make(user0, user1, 1000), true);
+	ASSERT_EQ(user1.GetBalance(), num1+1000);	
+	ASSERT_EQ(user0.GetBalance(), num0-1000-num_fee);
+	ASSERT_EQ(test_transaction.Make(user0, user1, 3900), false);
+	ASSERT_EQ(user1.GetBalance(), num1+1000);	
+	ASSERT_EQ(user0.GetBalance(), num0-1000-num_fee);
 }
 
 TEST(Account, Banking){
